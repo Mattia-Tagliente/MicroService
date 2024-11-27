db8f8e65c3cb8aff96172ccaab278b797d07d3ec
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-11-27 13:43:38 +0100

    Removes the not used library LocalDateTime

--- src/main/java/com/example/SupportTicketTechPlatform/entity/TicketStatus.java
+++ src/main/java/com/example/SupportTicketTechPlatform/entity/TicketStatus.java
@@ -1,7 +1,6 @@
 package com.example.SupportTicketTechPlatform.entity;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
