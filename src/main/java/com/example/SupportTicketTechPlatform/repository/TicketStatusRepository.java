6dbac39705cf9d78247ccd3cb735531d7499d50a
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-11-28 17:19:38 +0100

    Changes the returned entity to TicketStatus and its id to TicketStatusId

--- src/main/java/com/example/SupportTicketTechPlatform/repository/TicketStatusRepository.java
+++ src/main/java/com/example/SupportTicketTechPlatform/repository/TicketStatusRepository.java
@@ -2,8 +2,9 @@ package com.example.SupportTicketTechPlatform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.SupportTicketTechPlatform.entity.TicketStatus;
 import com.example.SupportTicketTechPlatform.entity.TicketStatusId;
 
-public interface TicketStatusRepository extends JpaRepository<TicketStatusId, Integer>{
+public interface TicketStatusRepository extends JpaRepository<TicketStatus, TicketStatusId>{
 
 }
