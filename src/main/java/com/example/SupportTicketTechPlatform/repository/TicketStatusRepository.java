a8947405f5f52693aaab00514c926596b87b23cd
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-11-28 21:37:01 +0100

    Adds a method to the TicketStatusRepository that finds the statuses of a ticket by its code

--- src/main/java/com/example/SupportTicketTechPlatform/repository/TicketStatusRepository.java
+++ src/main/java/com/example/SupportTicketTechPlatform/repository/TicketStatusRepository.java
@@ -1,10 +1,14 @@
 package com.example.SupportTicketTechPlatform.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.SupportTicketTechPlatform.entity.TicketStatus;
 import com.example.SupportTicketTechPlatform.entity.TicketStatusId;
 
 public interface TicketStatusRepository extends JpaRepository<TicketStatus, TicketStatusId>{
+
+    @Query("SELECT t FROM TicketStatus t WHERE ticket.ticketCode = ?1")
+    TicketStatus getTicketStatusByTicketCode(String ticketCode);
 
 }
