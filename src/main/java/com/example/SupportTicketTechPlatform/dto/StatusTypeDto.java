7af1598e11f403c5c05ca18e5930c5a1902f606e
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-11-28 20:49:18 +0100

    Corrects a typo: StatusType in dto package renamed StatusTypeDto

---
+++ src/main/java/com/example/SupportTicketTechPlatform/dto/StatusTypeDto.java
@@ -0,0 +1,20 @@
+package com.example.SupportTicketTechPlatform.dto;
+
+public class StatusTypeDto {
+
+    String statusName;
+    
+
+    public StatusTypeDto() {
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+    
+
+}
