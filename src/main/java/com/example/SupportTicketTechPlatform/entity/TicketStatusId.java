8a6c5c4397d2c0119160f57e82549dba2cba9808
Mattia-Tagliente <mattia.tagliente.mt@gmail.com>
2024-11-28 17:18:18 +0100

    Changes the types of the variables from int to StatusType and Ticket

--- src/main/java/com/example/SupportTicketTechPlatform/entity/TicketStatusId.java
+++ src/main/java/com/example/SupportTicketTechPlatform/entity/TicketStatusId.java
@@ -2,43 +2,51 @@ package com.example.SupportTicketTechPlatform.entity;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Embeddable
 public class TicketStatusId implements Serializable {
 
-    private int ticketId;
-    private int typeId;
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticketId;
+
+    @ManyToOne
+    @JoinColumn(name = "status_type", referencedColumnName = "type_id", nullable = false)
+    private StatusType typeId;
 
     public TicketStatusId() {}
 
-    public TicketStatusId(int ticketId, int typeId) {
+    public TicketStatusId(Ticket ticketId, StatusType typeId) {
         this.ticketId = ticketId;
         this.typeId = typeId;
     }
 
-    public int getTicketId() {
+    public Ticket getTicketId() {
         return ticketId;
     }
 
-    public void setTicketId(int ticketId) {
+    public void setTicketId(Ticket ticketId) {
         this.ticketId = ticketId;
     }
 
-    public int getTypeId() {
+    public StatusType getTypeId() {
         return typeId;
     }
 
-    public void setTypeId(int typeId) {
+    public void setTypeId(StatusType typeId) {
         this.typeId = typeId;
-    }
+    }    
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ticketId;
-        result = prime * result + typeId;
+        result = prime * result + ((ticketId == null) ? 0 : ticketId.hashCode());
+        result = prime * result + ((typeId == null) ? 0 : typeId.hashCode());
         return result;
     }
 
@@ -51,9 +59,15 @@ public class TicketStatusId implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         TicketStatusId other = (TicketStatusId) obj;
-        if (ticketId != other.ticketId)
+        if (ticketId == null) {
+            if (other.ticketId != null)
+                return false;
+        } else if (!ticketId.equals(other.ticketId))
             return false;
-        if (typeId != other.typeId)
+        if (typeId == null) {
+            if (other.typeId != null)
+                return false;
+        } else if (!typeId.equals(other.typeId))
             return false;
         return true;
     }
